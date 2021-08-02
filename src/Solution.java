2aac0a7ea97f621a848fc22e9db6efcbe4ab506e
unknown <lbvbxlbvbx@gmail.com>
2021-08-02 14:54:07 +0300

    first commit

---
+++ src/Solution.java
@@ -0,0 +1,13 @@
+public class Solution {
+    public static boolean hasCycle(ListNode head) {
+        ListNode slowPointer = head;
+        ListNode fastPointer = head;
+        while (fastPointer != null && fastPointer.next != null) {
+            slowPointer = slowPointer.next;
+            fastPointer = fastPointer.next.next;
+            if (slowPointer == fastPointer)
+                return true;
+        }
+        return false;
+    }
+}
